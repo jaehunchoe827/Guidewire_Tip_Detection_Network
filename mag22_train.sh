3158f1a35d2f9542bda10ee4e3a387f4abfd5ddc
jaehun choe <jaehunchoe827@gmail.com>
2026-04-24 13:22:07 -0400

    more configs

--- mag22_train.sh
+++ mag22_train.sh
@@ -4,4 +4,16 @@ echo "Training ver3_default.yaml"
 python3 -m engine.main --train --config ver3_default.yaml 
 sleep 3
 
+echo "Training ver3_default_unfreeze_3.yaml"
+python3 -m engine.main --train --config ver3_default_unfreeze_3.yaml 
+sleep 3
+
+echo "Training ver3_default_more_crop.yaml"
+python3 -m engine.main --train --config ver3_default_more_crop.yaml 
+sleep 3
+
+echo "Training ver3_default_unfreeze_3_warmup_1.yaml"
+python3 -m engine.main --train --config ver3_default_unfreeze_3_warmup_1.yaml 
+sleep 3
+
 echo "All models trained successfully!"
