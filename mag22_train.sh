8d226ce66278337b3cd4e3076e6a88f84a948032
jaehun choe <jaehunchoe827@gmail.com>
2026-04-29 23:36:57 -0400

    mag22 train gwtd updated

--- mag22_train.sh
+++ mag22_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Training ver3_default_ema_long_soft.yaml"
-python3 -m engine.main --train --config ver3_default_ema_long_soft.yaml 
+echo "Training default_epochs_75.yaml"
+python3 -m engine.main --train --config default_epochs_75.yaml 
 sleep 3
 
 echo "All models trained successfully!"
